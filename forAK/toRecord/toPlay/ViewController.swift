6659e72ec15a57085d510679c8bd72023f6a20ce
邓江洲 <707703386@qq.com>
2020-12-16 09:32:07 +0800

    recordBtn

--- forAK/toRecord/toPlay/ViewController.swift
+++ forAK/toRecord/toPlay/ViewController.swift
@@ -14,10 +14,10 @@ class ViewController: UIViewController {
     
     
     let engine = AudioEngine()
-    let player = AudioPlayer()
+
     
     
-    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var recordBtn: UIButton!
     
     
     
@@ -27,16 +27,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        player.isLooping = true
-        engine.output = player
-        do {
-            try engine.start()
-        } catch {
-            print("AudioKit did not start! \(error)")
-        }
-        
-        
-        
+    
+//        do {
+//            try engine.start()
+//        } catch {
+//            print("AudioKit did not start! \(error)")
+//        }
+//
+//
+//
  
     }
 
@@ -46,46 +45,19 @@ class ViewController: UIViewController {
     
     
     
-    @IBAction func toPlay(_ sender: Any) {
-        if player.isPlaying{
-            toPause()
-        }
-        else{
-            toPlay()
-        }
+    @IBAction func tapToRecord(_ sender: Any) {
+        
     }
     
     
-    func toPause(){
-        player.stop()
-        playBtn.setTitle("去播放", for: .normal)
+    func toStop(){
+        
     }
     
     
     
-    func toPlay(){
-        player.stop()
-        let url = Bundle.main.url(forResource: "1_Le Papillon", withExtension: "mp3")
+    func toRecord(){
         
-        var file: AVAudioFile?
-        
-        do {
-            file = try AVAudioFile(forReading: url!)
-        } catch {
-            print(error)
-        }
-        
-        
-        guard let f = file else {
-            return
-        }
-        lengthLabel.text = "音频时长是 \(f.duration.mmSS)"
-        
-        let buffer = try! AVAudioPCMBuffer(file: f)!
-        player.buffer = buffer
-        player.schedule(at: nil)
-        player.play()
-        playBtn.setTitle("在播放，去关掉", for: .normal)
     }
     
     
